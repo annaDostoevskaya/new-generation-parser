d75f6e5b44ffbf233cec131a1bd3c5d3d273fe50
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2020-06-01 14:14:10 +0600

    add config woth environ datas

--- open_site/olimp.py
+++ open_site/olimp.py
@@ -1,6 +1,9 @@
+import configparser
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 import sys
+import os, platform
 
 def check(str):
     if type(str) != type("string"):
@@ -48,8 +51,47 @@ def main(argv):
     assert check(link_to_league) == 0
     link_to_teams = get_teams(link_to_league, argv[2])
     assert check(link_to_teams) == 0
-    print("https://olimpbet.kz"+link_to_teams)
+    url = "https://olimpbet.kz"+link_to_teams
+    print(url)
+    return url
+    
+
+def get_koefs(url, exodus, koef):
+    soup = get_page(url)
+    table_of_koefs = soup.findAll('table', 
+            {'class' : 'koeftable2'})[0]
+    exodus_and_koefs = table_of_koefs.findAll('nobr')
+
+    return exodus_and_koefs
+
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    #url = main(sys.argv[1:])
+    # test in windows
+    if platform.system() == 'Windows':
+        mozilla_profile = os.path.join(os.getenv('APPDATA'), r'Mozilla\Firefox')
+        mozilla_profile_ini = os.path.join(mozilla_profile, r'profiles.ini')
+        profile_config = configparser.ConfigParser()
+        with open(mozilla_profile_ini, "r") as ini_file:
+            profile_config.read_file(ini_file)
+        path_to_profile = os.path.normpath(os.path.join(mozilla_profile,
+                                profile_config.get('Profile0', 'Path')))
+
+    elif platform.system() == 'Linux':
+        mozilla_profile = '/home/'+os.getenv('USER')+'/.mozilla/firefox/'
+        mozilla_profile_ini = mozilla_profile+'profiles.ini'
+        profile_config = configparser.ConfigParser()
+        with open(mozilla_profile_ini, "r") as ini_file:
+            profile_config.read_file(ini_file)
+        path_to_profile = mozilla_profile+profile_config.get('Profile0', 'Path')
+
+
+    profile = webdriver.FirefoxProfile(path_to_profile)
+    driver = webdriver.Firefox(profile)
+    
+    url = main(["Футбол", "Беларусь. Высшая лига. Статистика", 
+        "УГЛ Рух Брест"])
+    driver.get(url)
+    id_koef = get_id_koef(url, koef, exodus)
+    
